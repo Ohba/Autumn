9cc1bd6bc00b20b0e04bcbeb244928fddd1e9fc3
Dan Langford <dan.langford@mokinetworks.com>
2013-03-15 14:22:23 -0600

    adding batoo as a JPA impl option

--- sample/src/main/java/com/ohba/autumn/sample/pojo/Vehicle.java
+++ sample/src/main/java/com/ohba/autumn/sample/pojo/Vehicle.java
@@ -1,14 +1,20 @@
 package com.ohba.autumn.sample.pojo;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Vehicle {
+	
+	@Id
+	private Long id;
 
 	private String make;
 	private String model;
--- sample/src/main/java/com/ohba/autumn/sample/resource/VehicleResource.java
+++ sample/src/main/java/com/ohba/autumn/sample/resource/VehicleResource.java
@@ -1,28 +1,67 @@
 package com.ohba.autumn.sample.resource;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import org.batoo.jpa.JPASettings;
+import org.batoo.jpa.core.BatooPersistenceProvider;
+import org.postgresql.Driver;
+
+import com.google.common.collect.Maps;
 import com.ohba.autumn.sample.pojo.Vehicle;
 
 @Path("/cars")
 @Produces("application/json")
 public class VehicleResource {
+	
+	private EntityManager em;
 
+	EntityManager getEM(){
+		
+		if(this.em==null) {
+			Map<String, String> properties = Maps.newHashMap();
+			properties.put(JPASettings.JDBC_DRIVER, Driver.class.getName());
+			properties.put(JPASettings.JDBC_URL, "jdbc:postgresql://ec2-54-243-234-148.compute-1.amazonaws.com:5432/d6bibaokcf97t3?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory");
+			properties.put(JPASettings.JDBC_USER, "zortzjzdfebuui");
+			properties.put(JPASettings.JDBC_PASSWORD, "v_ySlclXd0pSHbzWKEuzXAbPLo");
+			//properties.put(BJPASettings.DDL, DDLMode.DROP.name());
+
+			EntityManagerFactory emf = new BatooPersistenceProvider().createEntityManagerFactory("batoo", properties , new String[]{
+					Vehicle.class.getName(), //
+			});
+
+			this.em = emf.createEntityManager();
+		}
+		
+		return this.em;
+	}
+	
     @GET
-    public Collection<Vehicle> getAll(){
-            return Arrays.asList(new Vehicle("Toyota","Tundra"), new Vehicle("Ford","Focus"), new Vehicle("Dodge","Dart"));
+    public List<Vehicle> getAll(){
+    	
+//    	getEM().getTransaction().begin();
+//    	
+//            List<Vehicle> cars = Arrays.asList(new Vehicle(100l,"Toyota","Tundra"), new Vehicle(200l,"Ford","Focus"), new Vehicle(300l,"Dodge","Dart"));
+//            for(Vehicle car : cars) {
+//            	getEM().persist(car);
+//            }
+//            
+//            getEM().getTransaction().commit();
+            
+            return getEM().createQuery("SELECT v FROM Vehicle v", Vehicle.class).getResultList();
     }
 
     @GET
-    @Path("/{make}")
-    public Vehicle getMake(@PathParam("make") String make){
-            return new Vehicle(make, null);
+    @Path("/{id}")
+    public Vehicle getMake(@PathParam("id") Long id){
+            return getEM().find(Vehicle.class, id);
     }
 	
 }
