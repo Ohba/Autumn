a765394bc37db0ce5a7b99c3d2a40dfc40695a7c
Faraday <Faraday@192.168.40.1>
2013-03-19 21:16:53 -0600

    some code cleanup. getting rid of a few maven warnings

--- src/test/java/com/ohba/autumn/NullAwareBeanUtilsTest.java
+++ src/test/java/com/ohba/autumn/NullAwareBeanUtilsTest.java
@@ -1,21 +1,16 @@
 package com.ohba.autumn;
 
-import static org.junit.Assert.assertEquals;
-
-import java.lang.reflect.InvocationTargetException;
-
-import lombok.val;
-
-import org.junit.Test;
-
 import com.ohba.autumn.utils.NullAwareBeanUtils;
+import java.lang.reflect.InvocationTargetException;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class NullAwareBeanUtilsTest {
 	
 	@Test
 	public void shouldNotCopyNull() throws IllegalAccessException, InvocationTargetException {
 		
-		val one = new TestBean(), two = new TestBean();
+		TestBean one = new TestBean(), two = new TestBean();
 		one.setFirstname("Danny");
 		two.setLastname("Langford");
 
