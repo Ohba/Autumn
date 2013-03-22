e84d170bc2ec7a1c565e9615197cf8b8577dff6f
danlangford <danlangford@cl0.beta.service.aws.koding.com>
2013-03-22 16:04:59 -0700

    a little bootstrap + ng polish

--- sample/src/main/webapp/public/js/myNG.js
+++ sample/src/main/webapp/public/js/myNG.js
@@ -6,22 +6,43 @@ factory('Car', function($resource) {
 });
 
 function CarCtrl($scope, Car) {
+    
+    function inputsEnabled(boo){
+        var elems = document.newCarForm.elements;
+        for(var e in elems){
+            elems[e].disabled=!boo;
+        }
+    }
+    
 	angular.extend($scope, {
 		
 		cars : Car.query(),
 		newCar : new Car(),
 		
 		create : function() {
+            var f = this.newCarForm;
+            inputsEnabled(false);
 			this.newCar.$save(function() {
-				$scope.cars = Car.query();
+                $scope.cars.push($scope.newCar);
+                $scope.newCar = new Car();
+                inputsEnabled(true);
 			});
 		},
 
 		remove : function(car) {
+            var id = car.id;
 			car.$remove(function() {
-				$scope.cars = Car.query();
+                for (i = 0; i < $scope.cars.length; ++i) {
+                    if ($scope.cars[i].id === id) {
+                        $scope.cars.splice(i--, 1);
+                    }
+                }
 			});
-		}
+		},
+        
+        reload : function(){
+            this.cars=Car.query();
+        }
 		
 	});
 };
