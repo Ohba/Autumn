755d43f6b3682a06e81c5ab559f867e05d1bc9de
Dan Langford <danlangford@gmail.com>
2013-03-24 20:19:33 -0600

    prepping some space for a no-sql example

---
+++ sample-nosql/src/main/java/com/ohba/autumn/sample/pojo/Vehicle.java
@@ -0,0 +1,25 @@
+package com.ohba.autumn.sample.pojo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Vehicle {
+	
+	@Id @GeneratedValue
+	private Long id;
+
+	private String make;
+	private String model;
+	
+	private String color;
+	
+}
---
+++ sample-nosql/src/main/java/com/ohba/autumn/sample/service/BogusService.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ohba.autumn.sample.service;
+
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ *
+ * @author kradolferap
+ */
+@Slf4j
+@ToString
+@NoArgsConstructor
+public class BogusService {
+    
+    public void message(String text){
+        log.info(text);
+    }
+}
