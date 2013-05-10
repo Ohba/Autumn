80f07fbd779f0d6627114ef1f1d4636975c5032d
Dan Langford <danlangford@gmail.com>
2013-05-10 09:03:35 -0600

    disable the security - stub out different security options

--- src/main/java/co/ohba/autumn/modules/AutumnShiroModule.java
+++ src/main/java/co/ohba/autumn/modules/AutumnShiroModule.java
@@ -1,8 +1,19 @@
 package co.ohba.autumn.modules;
 
 import co.ohba.autumn.AutumnConfig;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.crypto.hash.Sha512Hash;
 import org.apache.shiro.guice.web.ShiroWebModule;
+import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.realm.jdbc.JdbcRealm;
+import org.apache.shiro.subject.PrincipalCollection;
 
 import javax.servlet.ServletContext;
 
@@ -17,18 +28,52 @@ public class AutumnShiroModule extends ShiroWebModule {
 
     protected void configureShiroWeb() {
 
-        bindRealm().toInstance(buildSAR());
+        bindRealm().toInstance(
+                // buildSimple() // great for quick debugging
+                // buildJDBC() // should we use this if they are making JDBC connection
+                buildAthrzngRealm() // easiest for non-jdbc like Mongo?
+                );
 
-        addFilterChain("/*", AUTHC_BASIC);
+        // DISABLE FOR NOW
+        //addFilterChain("/*", AUTHC_BASIC);
 
     }
 
-    private SimpleAccountRealm buildSAR() {
+    private SimpleAccountRealm buildSimple() {
         SimpleAccountRealm sar = new SimpleAccountRealm();
         //              USER        PASS        ROLES ...
         sar.addAccount( "admin",    "admin",    "admin");
         sar.addAccount( "user",     "user",     "user","guest");
         return sar;
+    }
+
+    private JdbcRealm buildJdbc() {
+        JdbcRealm jr= new JdbcRealm();
+        // TODO fill this all out if you dont like their defaults
+        jr.setDataSource( /*...*/ null );
+        jr.setAuthenticationQuery( /*...*/ null);
+        jr.setUserRolesQuery( /*...*/ null);
+        // etc. and there are MORE
+        return jr;
+    }
+
+    private AuthorizingRealm buildAthrzngRealm() {
+        AuthorizingRealm ar = new AuthorizingRealm(new HashedCredentialsMatcher(Sha512Hash.ALGORITHM_NAME)) {
+            @Override
+            protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+                SimpleAuthorizationInfo sai = new SimpleAuthorizationInfo();
+                // ...
+                return sai;
+            }
+
+            @Override
+            protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+                SimpleAuthenticationInfo sai = new SimpleAuthenticationInfo();
+                // ...
+                return sai;
+            }
+        };
+        return ar;
     }
 
 }
